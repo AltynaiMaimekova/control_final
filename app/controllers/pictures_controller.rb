a8dba65e679619015c8129caab556d29cb70e696
Altynai <janarbek.akinai@gmail.com>
2018-01-29 16:44:08 +0600

    Form for uploading new photo was added to Place page

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,34 +1,38 @@
 class PicturesController < ApplicationController
-  before_action :set_picture, only: [:show, :edit, :update, :destroy]
+  before_action :set_picture, only: [:destroy]
+  before_action :set_place
 
   # GET /pictures
   # GET /pictures.json
-  def index
-    @pictures = Picture.all
-  end
+  # def index
+  #   @pictures = Picture.all
+  # end
 
   # GET /pictures/1
   # GET /pictures/1.json
-  def show
-  end
+  # def show
+  # end
 
   # GET /pictures/new
   def new
-    @picture = Picture.new
+    @picture = @place.pictures.build
   end
 
   # GET /pictures/1/edit
-  def edit
-  end
+  # def edit
+  # end
 
   # POST /pictures
   # POST /pictures.json
   def create
-    @picture = Picture.new(picture_params)
+    # @picture = Picture.new(picture_params)
+    @picture = @place.pictures.build(picture_params)
+    @picture.user_id = current_user.id
+    @picture.place_id = @place.id
 
     respond_to do |format|
       if @picture.save
-        format.html { redirect_to @picture, notice: 'Picture was successfully created.' }
+        format.html { redirect_to place_path(@place), notice: 'Picture was successfully created.' }
         format.json { render :show, status: :created, location: @picture }
       else
         format.html { render :new }
@@ -39,24 +43,24 @@ class PicturesController < ApplicationController
 
   # PATCH/PUT /pictures/1
   # PATCH/PUT /pictures/1.json
-  def update
-    respond_to do |format|
-      if @picture.update(picture_params)
-        format.html { redirect_to @picture, notice: 'Picture was successfully updated.' }
-        format.json { render :show, status: :ok, location: @picture }
-      else
-        format.html { render :edit }
-        format.json { render json: @picture.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @picture.update(picture_params)
+  #       format.html { redirect_to @picture, notice: 'Picture was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @picture }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @picture.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /pictures/1
   # DELETE /pictures/1.json
   def destroy
     @picture.destroy
     respond_to do |format|
-      format.html { redirect_to pictures_url, notice: 'Picture was successfully destroyed.' }
+      format.html { redirect_to place_path(@place), notice: 'Picture was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -66,9 +70,11 @@ class PicturesController < ApplicationController
     def set_picture
       @picture = Picture.find(params[:id])
     end
-
+    def set_place
+      @place = Place.find(params[:place_id])
+    end
     # Never trust parameters from the scary internet, only allow the white list through.
     def picture_params
-      params.require(:picture).permit(:place_id, :user_id)
+      params.require(:picture).permit(:place_id, :user_id, :image)
     end
 end
