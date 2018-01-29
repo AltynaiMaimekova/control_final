f6565d996b88873965939d302911ff44ca6b0eb7
Altynai <janarbek.akinai@gmail.com>
2018-01-29 17:03:38 +0600

    Form for creation of new place was upgraded

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -27,6 +27,7 @@ class PlacesController < ApplicationController
   # POST /places.json
   def create
     @place = Place.new(place_params)
+    @place.user_id = current_user.id
 
     respond_to do |format|
       if @place.save
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 	def average_score(place)
-		mean_score = ((place.reviews.average(:food) + place.reviews.average(:service) + place.reviews.average(:interior))/3).round(1)
+		if place.reviews.length != 0
+			mean_score = ((place.reviews.average(:food) + place.reviews.average(:service) + place.reviews.average(:interior))/3).round(1)
+		else
+			mean_score = 0
+		end
 	end
 end
